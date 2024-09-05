c0985eb48ee15b91888d2455c92c403ee7b0c029
csddl <nieci2024@163.com>
2024-09-06 00:31:04 +0800

    Update scripts_testimonials.js

--- js/scripts_testimonials.js
+++ js/scripts_testimonials.js
@@ -1,8 +1,7 @@
-// 加载学长说信息
 fetch('data/testimonials.json')
     .then(response => response.json())
     .then(data => {
-        const testimonialList = document.getElementById('testimonial-list');
+        const testimonialList = document.getElementById('timeline');  // 修改为 'timeline'
         data.sort((a, b) => new Date(b.time) - new Date(a.time));  // 按时间倒序排列
         data.forEach(testimonial => {
             const testimonialCard = document.createElement('div');
